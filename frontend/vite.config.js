2ea8efea5426b9e975e6c15e5e4bda4a108dffc3
CrisM05 <Max42113@gmail.com>
2024-08-16 04:01:07 -0400

    FRONT END HELP

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://0.0.0.0:3000",
+        target: process.env.api || "http://0.0.0.0:3000",
         changeOrigin: true,
         secure: false,
         ws: true,
